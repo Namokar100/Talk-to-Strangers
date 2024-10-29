0c417cfc7fe56fc81e310bccd0ee230e84d5b418
Namokar100 <namokarsvg@gmail.com>
2024-10-29 19:51:26 +0530

    additional controllers have been added

--- index.js
+++ index.js
@@ -15,8 +15,20 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 //routes
 const startRoutes = require("./routes/startRoutes")(bot);
 const userRoutes = require("./routes/userRoutes")(bot);
-const menuRoutes = require("./routes/menuRoutes")(bot);
+// const menuRoutes = require("./routes/menuRoutes")(bot);
+const findPartnerController = require("./controllers/findPartnerController");
 
+// Start Listening for Bot Commands
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  findPartnerController.showMenu(bot, chatId);
+});
+
+// Handle menu actions
+findPartnerController.handleMenuActions(bot);
+
+// Handle incoming messages
+findPartnerController.handleMessages(bot);
 
 // Start server
 const PORT = process.env.PORT || 3000;
